81cc917caa33d2ace0f3576db915616356d47cf3
DHYEY DESAI <dhyeydes@usc.edu>
2026-07-15 19:23:19 -0700

    Add: two-integer-sum - submission-9

---
+++ Data Structures & Algorithms/two-integer-sum/submission-9.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        #optimised
+        des = {}
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in des:
+                return [des[diff], i]
+            des[n] = i
